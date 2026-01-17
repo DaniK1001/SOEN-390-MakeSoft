62944353d80920a1e70b47573892bd3bc860196b
Massimo Paolini <mpaolini119@gmail.com>
2026-01-17 17:29:31 -0500

    Setup Maven and Spring Boot; Update README to Reflect Changes. Closes T-0.1.1, T-0.1.2

---
+++ backend-api/src/main/java/com/makesoft/app/AppApplication.java
@@ -0,0 +1,14 @@
+package com.makesoft.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// TEMPORARY FILE FOR BOOTSTRAP PURPOSES ONLY
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
