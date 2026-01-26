6eceb0ba2518c2262987f69bee553ee46666f18c
Ammar Ranko <ammar.ranko707@gmail.com>
2026-01-26 01:10:51 -0500

    Added Android and iOS google SDK Map API keys to the app configuration file to connect to the Google Cloud account.

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -2,6 +2,24 @@
 module.exports = ({ config }) => {
   return {
     ...config,
+    android: {
+      ...config.android,
+      config: {
+        ...config.android?.config,
+        googleMaps: {
+          apiKey: "AIzaSyBUU2yHJ8Fzw-4z7h7ueZuXIr4UXyZOMVY",
+        },
+      },
+    },
+
+    ios: {
+      ...config.ios,
+      config: {
+        ...config.ios?.config,
+        googleMapsApiKey: "AIzaSyBgq86SSQVcPItTBDY8YYbiEMSgMI3aM6c",
+      },
+    },
+
     extra: {
       PC_IP: process.env.EXPO_PUBLIC_PC_IP || process.env.REACT_NATIVE_PACKAGER_HOSTNAME || null,
     },
